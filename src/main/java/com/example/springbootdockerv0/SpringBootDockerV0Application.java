181483db17950b821ceca09167ec5d367838fbce
Dadapeer <abdulinfotec@gmail.com>
2024-02-13 13:33:12 +0530

    commit base

---
+++ src/main/java/com/example/springbootdockerv0/SpringBootDockerV0Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootdockerv0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerV0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerV0Application.class, args);
+	}
+
+}
